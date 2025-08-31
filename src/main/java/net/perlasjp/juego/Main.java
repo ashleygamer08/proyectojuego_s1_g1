db3cdb37109b80a477fac38c1711f913f5330fa5
Ashley <ashleyyesseniaflores@gmail.com>
2025-08-31 13:43:14 -0600

    Yessenia3

--- src/main/java/net/perlasjp/juego/Main.java
+++ src/main/java/net/perlasjp/juego/Main.java
@@ -1,7 +1,7 @@
 package net.perlasjp.juego;
 public class Main {
     public static void main(String[] args) {  
-        Juego juego = new Juego();
+        Juego juego = new juego();
         juego.iniciar ();
     }
 }
